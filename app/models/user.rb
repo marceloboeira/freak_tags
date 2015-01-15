74ef3ffc453943f88d58f8977486da854d436c10
Marcelo Boeira <me@marceloboeira.com>
2015-01-14 22:36:30 -0200

    Identation problems, after editorconfig

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
 	validates_presence_of :username, :email
 	validates_uniqueness_of :username, :email
-  validates_length_of :username, :name, :minimum => 3, :maximum => 50
-  validates_length_of :email, :minimum => 5, :maximum => 50
+	validates_length_of :username, :name, :minimum => 3, :maximum => 50
+	validates_length_of :email, :minimum => 5, :maximum => 50
 	acts_as_birthday :birthday
 
 	def age
