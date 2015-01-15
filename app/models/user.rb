fd7df1b630e11361cf51b943740eb8fb7016eea2
Marcelo Boeira <me@marceloboeira.com>
2015-01-15 16:26:41 -0200

    validations orders, and some fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ActiveRecord::Base
 
-  validates_presence_of :username, :email
-  validates_uniqueness_of :username, :email
-  validates_length_of :username, :name, :minimum => 3, :maximum => 50
-  validates_length_of :email, :minimum => 5, :maximum => 50
   acts_as_birthday :birthday
   has_enumeration_for :sex, :with => Sex, :create_helpers => true
+
+  validates_presence_of :name, :username, :email
+  validates_uniqueness_of :username, :email
+  validates_length_of :name, :username, :minimum => 3, :maximum => 50
+  validates_length_of :email, :minimum => 5, :maximum => 50
 
   def age
     birthday_age
