415b0131b2d0e33d8fd7363b0368df81290b9efd
Marcelo Boeira <me@marceloboeira.com>
2015-01-15 14:46:15 -0200

    solving identation problems...

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ActiveRecord::Base
 
-	validates_presence_of :username, :email
-	validates_uniqueness_of :username, :email
-	validates_length_of :username, :name, :minimum => 3, :maximum => 50
-	validates_length_of :email, :minimum => 5, :maximum => 50
-	acts_as_birthday :birthday
-	has_enumeration_for :sex, :with => Sex, :create_helpers => true
+  validates_presence_of :username, :email
+  validates_uniqueness_of :username, :email
+  validates_length_of :username, :name, :minimum => 3, :maximum => 50
+  validates_length_of :email, :minimum => 5, :maximum => 50
+  acts_as_birthday :birthday
+  has_enumeration_for :sex, :with => Sex, :create_helpers => true
 
-	def age
-		birthday_age
-	end
+  def age
+    birthday_age
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
   resources :users
 
 end
