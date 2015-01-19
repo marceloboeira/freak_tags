f1a132ac0fc731c5a05ed14aa8e7d9ca343c1a0e
Marcelo Boeira <me@marceloboeira.com>
2015-01-19 21:46:35 -0200

    [#17] Database not null and default values

--- db/migrate/20150114195436_create_users.rb
+++ db/migrate/20150114195436_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :username
-      t.string :email
-      t.string :gender
+      t.string :name,  :null => false
+      t.string :username,  :null => false
+      t.string :email, :null => false
+      t.string :gender,  :null => false, :default => 'male'
       t.text :description
       t.date :birthday
       t.timestamps
--- db/migrate/20150118110146_create_friendships.rb
+++ db/migrate/20150118110146_create_friendships.rb
@@ -1,9 +1,9 @@
 class CreateFriendships < ActiveRecord::Migration
   def change
     create_table :friendships do |t|
-      t.integer :user_id
-      t.integer :friend_id
-      t.string :status
+      t.integer :user_id, :null => false
+      t.integer :friend_id, :null => false
+      t.string :status, :null => false, :default => 'pending'
 
       t.timestamps
     end
