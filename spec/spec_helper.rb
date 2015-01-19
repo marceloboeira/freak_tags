094156de5cdb7ea987ac6d96b5702becfacb8891
Marcelo Boeira <me@marceloboeira.com>
2015-01-19 09:12:47 -0200

    [#38] Shoulda && test refactoring

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,8 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'capybara/rspec'
+require 'shoulda/matchers'
+require 'faker'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
