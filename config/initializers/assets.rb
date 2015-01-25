870f758ddfb9ba0783fd84028c0e8d1d6615ba3a
Marcelo Boeira <me@marceloboeira.com>
2015-01-25 17:58:58 -0200

    [#49][#41] Assets ext typos

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( core.css )
 Rails.application.config.assets.precompile += %w( core.js )
 Rails.application.config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif]
-Rails.application.config.assets.precompile += %w[*.tiff *.wolf *.ttf *.svg *.otf]
+Rails.application.config.assets.precompile += %w[*.woff *.ttf *.svg *.otf *.eot]
