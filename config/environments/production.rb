58205dc534676acd8128782bc1e9d04964df5c60
Marcelo Boeira <me@marceloboeira.com>
2015-01-30 19:59:04 -0200

    [#53] Heroku is F#0 me

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   config.cache_classes = true
   config.eager_load = true
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
   config.serve_static_files = true
   config.assets.js_compressor = :uglifier
