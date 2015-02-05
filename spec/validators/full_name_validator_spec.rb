472d7f4d49abc81490ba58faa0710d9b0d2c7584
Marcelo Boeira <me@marceloboeira.com>
2015-02-05 09:56:09 -0200

    [#87] Testing validators

---
+++ spec/validators/full_name_validator_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+class FakeModel
+  include ActiveModel::Validations
+  attr_accessor :name
+  validates :name, :full_name => true
+end
+
+describe FullNameValidator do
+
+  let (:fake_model) { FakeModel.new }
+
+  it "accepts normal names, dr., mr. and special chars" do
+    expect(fake_model).to allow_value("Jerry Seinfeld", "Elaine Baines", "Cosmo Kramer", "Dr. Assman", "Mr. Constanza", "Kramer O'boo").for(:name)
+  end
+
+  it "rejects invalid names / invalid chars" do
+    expect(fake_model).to_not allow_value("El@ine", "Kramer O\"boo", "KRAMER", "Mr, contanza", "SoUpNa zi").for(:name)
+  end
+end
---
+++ spec/validators/username_validator_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+class FakeModel
+  include ActiveModel::Validations
+  attr_accessor :username
+  validates :username, :username => true
+end
+
+describe UsernameValidator do
+
+  let (:fake_model) { FakeModel.new }
+
+  it "accepts underscore, hyphen, uppercase, lowercase and numbers" do
+    expect(fake_model).to allow_value("jerry_seinfeld", "elaine_baines_9", "_CosMo23", "_newman_7","george-constanza", "UncLE-LeO-", "-CosmO-KrameR", "George_Cons-tanza99", "elaINE_-baines").for(:username)
+  end
+
+  it "rejects url unsafe chars, dots" do
+    expect(fake_model).to_not allow_value("!george", "elaine#baines", "jerry%9", "ass^man", "kram.3r", "el@ine", "$constanza", "newman*", "soup?").for(:username)
+  end
+end
