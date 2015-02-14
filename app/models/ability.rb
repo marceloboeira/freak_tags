2ac6666fc1ba02c68c27d8897bb9a7d8328c43ba
Marcelo Boeira <me@marceloboeira.com>
2015-02-14 21:16:18 -0200

    [#80] Getting news + cancancan to work ...scratch onyl

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,10 @@ class Ability
   def initialize(user)
     user ||= User.new
     can :read, User
+    can :read, News
+
+    can :create, News if user.contributor?
+    can :manage, News if user.moderator?
     can :manage, :all if user.root?
   end
 end
