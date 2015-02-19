a97eae20782f7b23b34f646fa02261863bb7a382
Marcelo Boeira <me@marceloboeira.com>
2015-02-19 09:42:06 -0200

    [#80] Identing, news seeds ...

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,30 @@
+# Basic needs to run it without headaches
 
 # Sample Users
-root = User.create(username: 'root', email: 'root@freaktags.com', password: 'seinfeld23', name: 'Root', birthday: Date.parse('1994-03-07'), gender: Gender::MALE, confirmed_at: DateTime.now, role: UserRole::ROOT)
-test = User.create(username: 'test', email: 'test@freaktags.com', password: 'smallville23', name: 'Test', birthday: Date.parse('1996-02-16'), gender: Gender::FEMALE, confirmed_at: DateTime.now, role: UserRole::REGULAR)
+root = User.create(username: 'root',
+                   email: 'root@freaktags.com',
+                   password: 'seinfeld23',
+                   name: 'Root',
+                   birthday: Date.parse('1994-03-07'),
+                   gender: Gender::MALE,
+                   confirmed_at: DateTime.now,
+                   role: UserRole::ROOT)
 
-# Sample friendship
+test = User.create(username: 'test',
+                   email: 'test@freaktags.com',
+                   password: 'smallville23',
+                   name: 'Test',
+                   birthday: Date.parse('1996-02-16'),
+                   gender: Gender::FEMALE,
+                   confirmed_at: DateTime.now,
+                   role: UserRole::REGULAR)
+
+# Sample Friendship
 root.invite test
 test.approve root
+
+# Sample News
+news_hello = News.create(author: root,
+                         title: 'Hello FreakTags News',
+                         slug_line: 'This is just a news example to populate FreakTags Database',
+                         content: '<h3>Hello Freaktaggers</h3> <br> Lets have some fun!')
