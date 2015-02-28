de0cba885c1390dbcb877d4aec6eb01969234068
Marcelo Boeira <me@marceloboeira.com>
2015-02-28 11:16:06 -0300

    [#110] Permissions test for news - DONE!

--- spec/features/news_spec.rb
+++ spec/features/news_spec.rb
@@ -2,73 +2,96 @@ require "spec_helper"
 
 describe "News", :type => :feature, :js => true  do
   let (:dexter) { create(:confirmed_user, role: UserRole::MODERATOR) }
+  let (:rita) { create(:confirmed_user, role: UserRole::CONTRIBUTOR) }
   let (:debra) { create(:confirmed_user, role: UserRole::REGULAR) }
   let (:killer_news) { create(:news, author: dexter) }
 
-  describe "regular user can" do
-    before :each do
-      login_as(debra)
-    end
-    it "view" do
-      visit news_path(killer_news)
-      expect(page).to have_content(killer_news.title)
-    end
-    it "not create, update or delete" do
-      visit new_news_path
-      [new_news_path, edit_news_path(killer_news),destroy_news_path(killer_news)].each do
-        expect(current_path).to eq dashboard_path
-        expect(page).to have_content("You are not authorized to access this page.")
+  describe "permissions" do
+    describe "regular" do
+      before :each do
+        login_as(debra)
+      end
+      it "is allowed to view" do
+        view_news
+      end
+      it "is not allowed to create, update or delete" do
+        check_unauthorized([new_news_path, edit_news_path(killer_news),destroy_news_path(killer_news)])
       end
     end
-  end
-  describe "moderator user can" do
-    before :each do
-      before_date = DateTime.now
-      login_as(dexter)
-    end
-    it "create" do
-      visit new_news_path
-      within("form.new_news") do
-        fill_in "Title", with: "The IceTruck Killer strikes again"
-        fill_in "Slug line", with: "Miami police confirm that the IceTruck Killer, AKA ICT, has killed again"
-        fill_in "Content", with: "<b> Kill'em All </b>"
-        click_on "Create"
+    describe "contributor" do
+      before :each do
+        login_as(rita)
       end
-      news = News.all.last
-      expect(news.created_at).to be_between(5.seconds.ago, DateTime.now)
-      expect(news.title).to have_content("IceTruck Killer")
-      expect(current_path).to eq(news_path(news))
-    end
-    it "view" do
-      visit news_path(killer_news)
-      expect(page).to have_content(killer_news.title)
-    end
-    it "update" do
-      new_slug_line = "tonight is the night, and is going to happen again, it has to happen..."
-      visit edit_news_path(killer_news)
-      within("form.edit_news") do
-        fill_in "Slug line", with: new_slug_line
-        click_on "Update"
+      it "is allowed to view and create" do
+        view_news
+        create_news(rita)
       end
-      killer_news.reload
-      expect(killer_news.slug_line).to eq(new_slug_line)
-      expect(killer_news.updated_at).to be_between(5.seconds.ago, DateTime.now)
-      expect(current_path).to eq(news_path(killer_news))
+      it "is not allowed to update and delete" do
+        check_unauthorized([edit_news_path(killer_news),destroy_news_path(killer_news)])
+      end
     end
-    it "destroy" do
-      visit destroy_news_path(killer_news)
-      killer_news.reload
-      expect(killer_news).to be_deleted
-      expect(killer_news.deleted_at).to be_between(5.seconds.ago, DateTime.now)
-      expect(current_path).to eq news_index_path
+    describe "moderator" do
+      before :each do
+        before_date = DateTime.now
+        login_as(dexter)
+      end
+      it "is allowed to create, view, update, destroy" do
+        create_news(dexter)
+        view_news
+        update_news
+        destroy_news
+      end
     end
   end
 
   private
 
-  def create_news
+  def create_news(user)
+    visit new_news_path
+    within("form.new_news") do
+      fill_in "Title", with: "The IceTruck Killer strikes again"
+      fill_in "Slug line", with: "Miami police confirm that the IceTruck Killer, AKA ICT, has killed again"
+      fill_in "Content", with: "<b> Kill'em All </b>"
+      click_on "Create"
+    end
+    news = News.all.last
+    expect(news.created_at).to be_between(5.seconds.ago, DateTime.now)
+    expect(news.title).to have_content("IceTruck Killer")
+    expect(news.author).to eq(user)
+    expect(current_path).to eq(news_path(news))
+  end
+
+  def view_news
+    visit news_path(killer_news)
+    expect(page).to have_content(killer_news.title)
   end
 
   def update_news
+    new_slug_line = "tonight is the night, and is going to happen again, it has to happen..."
+    visit edit_news_path(killer_news)
+    within("form.edit_news") do
+      fill_in "Slug line", with: new_slug_line
+      click_on "Update"
+    end
+    killer_news.reload
+    expect(killer_news.slug_line).to eq(new_slug_line)
+    expect(killer_news.updated_at).to be_between(5.seconds.ago, DateTime.now)
+    expect(current_path).to eq(news_path(killer_news))
+  end
+
+  def destroy_news
+    visit destroy_news_path(killer_news)
+    killer_news.reload
+    expect(killer_news).to be_deleted
+    expect(killer_news.deleted_at).to be_between(5.seconds.ago, DateTime.now)
+    expect(current_path).to eq news_index_path
+  end
+
+  def check_unauthorized(paths = [])
+    paths.each do |path|
+      visit path
+      expect(current_path).to eq dashboard_path
+      expect(page).to have_content("You are not authorized to access this page.")
+    end
   end
 end
