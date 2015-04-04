db3379960821ae6e5d513491721461b5edf5f8fc
Marcelo Boeira <me@marceloboeira.com>
2015-04-04 18:54:27 -0300

    Improvements at user_path/profile_path because of devise

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,9 +5,13 @@ module UsersHelper
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     classes = " "
-    classes << options.delete(:class)
+    classes << options.delete(:class) if options.key? :class
     classes << " gravatar"
     id = options.delete(:id)
     image_tag(gravatar_url, title: user.username, class: classes.to_s, id: id)
+  end
+
+  def user_path user
+    profile_path user.username
   end
 end
