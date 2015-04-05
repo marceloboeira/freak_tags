78bcae8267b0051072876695b0aa7f45c7e6fc9f
Marcelo Boeira <me@marceloboeira.com>
2015-04-05 16:05:47 -0300

    No need for https with gravatar, its faster whitout it

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,7 @@ module UsersHelper
   ## While paperclip isn't ready, let get it from gravatar
   def gravatar_for(user, options = {})
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}"
     classes = " "
     classes << options.delete(:class) if options.key? :class
     classes << " gravatar"
