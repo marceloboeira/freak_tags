faf12ff39d52ee70e3265c88c1b6dc0c961e3b31
Marcelo Boeira <me@marceloboeira.com>
2015-04-05 13:25:35 -0300

    [#18] Friends activities only

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class DashboardController < ApplicationController
   def index
-    @activities = PublicActivity::Activity.order("created_at desc")
+    @activities = PublicActivity::Activity.where(owner_id: current_user.friends.map(&:id)).order("created_at desc")
   end
 end
