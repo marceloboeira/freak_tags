d9b88c6361ccc6349b397773b76626ff40218cd3
Marcelo Boeira <me@marceloboeira.com>
2015-04-20 17:18:15 -0300

    [#153] Starting to track following activities ...

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -2,6 +2,8 @@ class Follow < ActiveRecord::Base
 
   extend ActsAsFollower::FollowerLib
   extend ActsAsFollower::FollowScopes
+  include PublicActivity::Model
+  tracked only: [:create], owner: :follower
 
   belongs_to :followable, :polymorphic => true
   belongs_to :follower,   :polymorphic => true
