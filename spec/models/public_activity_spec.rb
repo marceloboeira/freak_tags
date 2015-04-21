b8acc221f28196b29eab6d9eec6dd941460fef3d
Marcelo Boeira <me@marceloboeira.com>
2015-04-21 00:32:57 -0300

    [#153] Testing out a bit, ...

--- spec/models/public_activity_spec.rb
+++ spec/models/public_activity_spec.rb
@@ -1,42 +1,58 @@
 require "spec_helper"
 
-
-PublicActivity.with_tracking do
-  describe "PublicActivity::Activity" do
-    let (:sherlock) { create(:root_user, username: "sherlock-holmes") }
-    let (:watson) { create(:root_user, username: "john-watson") }
-    let (:moriarty) { create(:root_user, username: "james-moriarty") }
-
-    before :each do
-      watson.follow sherlock
-      sherlock.follow watson
-
-      # It must be created here in order to create PublicActivity records, don't know why exactly
-      @first_news = create(:news, author: sherlock)
-      @second_news = create(:news, author: watson)
-      @activities = PublicActivity::Activity.all
+describe "PublicActivity::Activity" do
+  let (:sherlock) { create(:root_user, username: "sherlock-holmes") }
+  let (:watson) { create(:root_user, username: "john-watson") }
+  let (:moriarty) { create(:root_user, username: "james-moriarty") }
+  before :each do
+    sherlock.follow watson
+    watson.follow sherlock
+    # It must be created here in order to create PublicActivity records, don't know why exactly
+    @first_news = create(:news, author: sherlock)
+    @second_news = create(:news, author: watson)
+    @activities = PublicActivity::Activity.all
+  end
+  describe "for news validates" do
+    it "attributes and types" do
+      # Follow
+      [@activities.first, @activities.second].each do |activity|
+        expect(activity.trackable.class).to be Follow
+      end
+      expect(@activities.first.trackable.followable).to eq watson
+      expect(@activities.second.trackable.followable).to eq sherlock
+      # News
+      [@activities.third, @activities.last].each do |activity|
+        expect(activity.trackable.class).to be News
+      end
+      expect(@activities.third.owner).to eq(sherlock)
+      expect(@activities.third.trackable).to eq(@first_news)
+      expect(@activities.last.owner).to eq(watson)
+      expect(@activities.last.trackable).to eq(@second_news)
+      @activities.each do |activity|
+        expect(activity.created_at).to be_between(5.seconds.ago, DateTime.now)
+      end
     end
-    describe "validates" do
-      it "attributes" do
-        expect(@activities.first.owner).to eq(sherlock)
-        expect(@activities.first.trackable).to eq(@first_news)
-        expect(@activities.last.owner).to eq(watson)
-        expect(@activities.last.trackable).to eq(@second_news)
-        @activities.each do |activity|
-          expect(activity.created_at).to be_between(5.seconds.ago, DateTime.now)
-        end
+    it "self activities" do
+      [@activities.first, @activities.third].each do |activity|
+        expect(sherlock.activities).to include activity
+        expect(watson.activities).to_not include activity
       end
-      it "self activities" do
-        expect(sherlock.activities).to include @activities.first
-        expect(sherlock.activities).to_not include @activities.last
-        expect(watson.activities).to include @activities.last
-        expect(watson.activities).to_not include @activities.first
+      [@activities.second, @activities.last].each do |activity|
+        expect(watson.activities).to include activity
+        expect(sherlock.activities).to_not include activity
       end
-      it "follows activities" do
-        expect(sherlock.follows_activities).to include @activities.last
-        expect(watson.follows_activities).to include @activities.first
-        expect(moriarty.follows_activities).to_not include @activities
+      expect(moriarty.activities).to be_empty
+    end
+    it "follows activities" do
+      [@activities.first, @activities.third].each do |activity|
+        expect(sherlock.follows_activities).to_not include activity
+        expect(watson.follows_activities).to include activity
       end
+      [@activities.second, @activities.last].each do |activity|
+        expect(watson.follows_activities).to_not include activity
+        expect(sherlock.follows_activities).to include activity
+      end
+      expect(moriarty.follows_activities).to be_empty
     end
   end
 end
