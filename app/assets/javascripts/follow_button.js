d7b9502ab00f13d7259f00089a9ed58141185f49
Marcelo Boeira <me@marceloboeira.com>
2015-04-21 20:50:42 -0300

    [#152] Standard method name ...commenting out

--- app/assets/javascripts/follow_button.js
+++ app/assets/javascripts/follow_button.js
@@ -6,11 +6,15 @@ var FollowButton = {
   selector: "[data-follow-button]",
   target: "[data-follow-count-id=?]",
 
+  /**
+   * Set main function for each button and main events
+   * @return {[type]} [description]
+   */
   init: function() {
     $(this.selector).each(function() {
       var self = $(this);
 
-      FollowButton._padronize(self);
+      FollowButton._standardize(self);
       self.on("click", function(event){
         event.preventDefault();
         var self = $(this),
@@ -19,22 +23,27 @@ var FollowButton = {
 
         self.data("following", !isFollowing);
         $.get(url, function(followers) {
-          FollowButton._padronize(self);
+          FollowButton._standardize(self);
           FollowButton._updateTarget(self, followers);
         });
       });
     });
   },
 
+  /**
+   * Update target counter
+   */
   _updateTarget: function(self, value) {
     var target = $(this.target.replace("?", self.data("followable-id")));
     target.html(value);
   },
 
-  _padronize: function(self) {
+  /**
+   * Standardize the button input
+   */
+  _standardize: function(self) {
     var isFollowing = self.data("following") == true;
     self.html(_.unescape(self.data(isFollowing ? "unfollow-content" : "follow-content")));
     self.attr("href",self.data(isFollowing ? "unfollow-url" : "follow-url"));
   }
-
 };
