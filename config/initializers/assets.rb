b666bb25627615936e5fd2424bd323327066da5a
Marvin Medeiros <marvin.medeiros52@gmail.com>
2015-04-22 03:06:15 -0300

    [#147] Add landing js to the compile list

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,3 +2,4 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( core.css )
 Rails.application.config.assets.precompile += %w( landing.css )
 Rails.application.config.assets.precompile += %w( core.js )
+Rails.application.config.assets.precompile += %w( landing.js )
