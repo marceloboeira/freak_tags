92b0676d504ba5fb679c16509ccdb05efc06e89e
Marcelo Boeira <me@marceloboeira.com>
2015-04-22 22:05:39 -0300

    FreakTags Object on a especific file

--- app/assets/javascripts/core.js
+++ app/assets/javascripts/core.js
@@ -1,37 +1,7 @@
-//= require underscore
-//= require jquery/jquery
-//= require bootstrap/bootstrap
-//
-//= require datetime_ago
-//= require follow_button
-//= require modal
-//= require html_editor
+//= require freaktags
 //= require_self
 
 $(function(){
-  /**
-   * FreakTags Front-end Core
-   * @type {Object}
-   */
-  var FreakTags = {
-    version: $("#freaktags-version").val(),
-    locale: $("#freaktags-locale").val(),
-    CSRF: $("meta[name=csrf-token]").attr("content"),
-
-    /**
-     * Booting up front-end core
-     * @return {Function}
-     */
-    init: function() {
-      $("[title]").tooltip();
-      DateTimeAgo.init(this.locale);
-      FollowButton.init();
-      Modal.init();
-      HtmlEditor.init();
-
-    }
-  };
-
   // Booting up core
   FreakTags.init();
 });
---
+++ app/assets/javascripts/freaktags.js
@@ -0,0 +1,29 @@
+//= require underscore
+//= require jquery/jquery
+//= require bootstrap/bootstrap
+//= require datetime_ago
+//= require follow_button
+//= require modal
+//= require html_editor
+
+/**
+ * FreakTags Front-end Core
+ * @type {Object}
+ */
+var FreakTags = {
+  version: document.getElementById("freaktags-version").value,
+  locale: document.getElementById("freaktags-locale").value,
+  CSRF: $("meta[name=csrf-token]").attr("content"),
+
+  /**
+   * Booting up front-end core
+   * @return {Function}
+   */
+  init: function() {
+    $("[title]").tooltip();
+    DateTimeAgo.init(this.locale);
+    FollowButton.init();
+    Modal.init();
+    HtmlEditor.init();
+  }
+};
