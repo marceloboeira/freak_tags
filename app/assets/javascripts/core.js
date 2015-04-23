b2bbc8d457b3589074f7f3f9a79ae3cb2f4fec59
Marcelo Boeira <me@marceloboeira.com>
2015-04-22 21:42:25 -0300

    Refactoring core and modal to abstract independent components 3x :clap:

--- app/assets/javascripts/core.js
+++ app/assets/javascripts/core.js
@@ -1,11 +1,10 @@
 //= require underscore
 //= require jquery/jquery
 //= require bootstrap/bootstrap
-//= require bootbox/bootbox
-//= require moment/moment
-//= require moment/locale/pt-br
 //
+//= require datetime_ago
 //= require follow_button
+//= require modal
 //= require html_editor
 //= require_self
 
@@ -24,60 +23,12 @@ $(function(){
      * @return {Function}
      */
     init: function() {
-      this._momentInit();
-      this._tooltipInit();
-
-      FollowButton.init();
-      HtmlEditor.init();
-    },
-
-    /**
-     * MomentJS Locale and live update settings
-     * @return {Function}
-     */
-    _momentInit: function() {
-      moment.locale(FreakTags.locale);
-      FreakTags._momentUpdate();
-      setInterval(FreakTags._momentUpdate, 60000);
-    },
-
-    /**
-     * MomentJS Live update on date/time inputs
-     * @return {Function}
-     */
-    _momentUpdate: function(){
-      $("date, time").each(function(i, e) {
-        var d = moment.unix($(e).data("source"));
-        $(e).html(d.fromNow());
-      });
-    },
-
-    /**
-     * Bootbox buttons init
-     * @return {[type]} [description]
-     */
-    _bootboxInit: function() {
-
-      /* Delete button */
-      $("a[data-destroy]").on("click", function (e){
-        var self = $(this);
-        var link = self.attr("href");
-        var message = self.data("destroy-message");
-        e.preventDefault();
-        bootbox.confirm(message, function(result){
-          if (result) {
-            window.location.href = link;
-          }
-        });
-      });
-    },
-
-    /**
-     * Bootstrap tooltip ini
-     * @return {function}
-     */
-    _tooltipInit: function() {
       $("[title]").tooltip();
+      DateTimeAgo.init(this.locale);
+      FollowButton.init();
+      Modal.init();
+      HtmlEditor.init();
+
     }
   };
 
