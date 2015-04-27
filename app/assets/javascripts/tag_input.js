12cf7bff23ff9005bcedcd1c839aa6c29af98a30
Marcelo Boeira <me@marceloboeira.com>
2015-04-26 21:14:47 -0300

    [#116] Form and show inputs :)

--- app/assets/javascripts/tag_input.js
+++ app/assets/javascripts/tag_input.js
@@ -1,10 +1,11 @@
 //= require bootstrap-tagsinput
 
-
 var TagInput = {
   selector: 'input.tags',
 
   init: function() {
-    $(this.selector).tagsinput();
+    $(this.selector).tagsinput({
+      tagClass: "label label-primary"
+    });
   }
 }
